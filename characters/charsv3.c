d956a5ec51982f4919b83168ec50d8a082e92d3f
Michael Ramirez <jxc876@gmail.com>
2025-06-19 14:55:22 -0500

    null terminate

--- characters/charsv3.c
+++ characters/charsv3.c
@@ -78,17 +78,22 @@ Result *analyze_string(const char *str) {
             letter_idx++;
         }
 
-        // add to digits array
+        // add to numbers array
         if (isdigit(str[i])) {
             result->numbers[num_idx] = str[i];
             num_idx++;
         }
 
-        // add to digits array
+        // add to special char array
         if (!isalnum(str[i]) && !isspace(str[i])) {
             result->special[special_idx] = str[i];
             special_idx++;
         }
     }
+
+    // Add null-terminators
+    result->letters[letter_idx] = '\0';
+    result->numbers[num_idx] = '\0';
+    result->special[special_idx] = '\0';
     return result;
 }
